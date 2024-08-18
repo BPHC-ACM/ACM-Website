a407843ce71812c826e897d41458ec093bbcb3cc
catfo435 <pranavlsgreat210120@gmail.com>
2024-08-18 19:15:10 +0530

    minor frontend fixes

--- Front-End/astro.config.mjs
+++ Front-End/astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
-import swiper from '@swiper/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  integrations: [swiper()],
-  integrations: [tailwind()]
+  integrations: [react(), tailwind()]
 });

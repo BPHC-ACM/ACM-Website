9f12a926ffaab9364b21356c64cdc42b617eae38
catfo435 <pranavlsgreat210120@gmail.com>
2024-08-23 21:58:07 +0530

    FAQ section done

---
+++ Front-End/src/data/faq.js
@@ -0,0 +1,5 @@
+export const faqItems = [
+    { question: "When are the inductions open?", answer: "ACM BPHC holds inductions once a semester. The inductions are generally start in the end of the first month of the semester." },
+    { question: "How can I get involved if I'm new to programming?", answer: "ACM BPHC is beginner-friendly and offers workshops and mentorship programs specifically designed for newcomers." },
+    { question: "What are the events ACM BPHC conducts?", answer: "Check out the events page to know what events we hold." }
+  ];

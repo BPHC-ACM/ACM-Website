96034256fbc1b06207f275297d8c2d77c55988b1
catfo435 <pranavlsgreat210120@gmail.com>
2024-08-23 20:57:04 +0530

    Meet the team finished, por photos pending

---
+++ Front-End/src/data/teamheads.js
@@ -0,0 +1,47 @@
+export const teamHeads = [
+    {
+        name : "Shashwat Sharma",
+        designation: "Chairperson",
+        image : "/acm logo png.png"
+    },
+    {
+        name : "Dhairya Luthra",
+        designation: "Vice Chairperson and App Dev Chair",
+        image : "/acm logo png.png"
+    },
+    {
+        name : "Vivan Jain",
+        designation: "Quant Chair",
+        image : "/acm logo png.png"
+    },
+    {
+        name : "Samarth Pawan",
+        designation: "ML Chair",
+        image : "/acm logo png.png"
+    },
+    {
+        name : "Arinjay Jain",
+        designation: "Web Dev Chair",
+        image : "/acm logo png.png"
+    },
+    {
+        name : "Tushar Anand",
+        designation: "Generative AI Chair",
+        image : "/acm logo png.png"
+    },
+    {
+        name : "Divya Daga",
+        designation: "Content Chair",
+        image : "/acm logo png.png"
+    },
+    {
+        name : "Majji Sai Snigdha",
+        designation: "Events Chair",
+        image : "/acm logo png.png"
+    },
+    {
+        name : "Lakshita Jain",
+        designation: "Design Chair",
+        image : "/acm logo png.png"
+    }
+]

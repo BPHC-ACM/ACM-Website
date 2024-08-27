c4d109059dac5ff9d1048e6206138c6ec89d76e7
catfo435 <pranavlsgreat210120@gmail.com>
2024-08-28 01:35:03 +0530

    Content update, css fixes

--- Front-End/src/data/faq.js
+++ Front-End/src/data/faq.js
@@ -1,5 +1,6 @@
 export const faqItems = [
-    { question: "When are the inductions open?", answer: "ACM BPHC holds inductions once a semester. The inductions are generally start in the end of the first month of the semester." },
-    { question: "How can I get involved if I'm new to programming?", answer: "ACM BPHC is beginner-friendly and offers workshops and mentorship programs specifically designed for newcomers." },
-    { question: "What are the events ACM BPHC conducts?", answer: "Check out the events page to know what events we hold." }
+    { question: "What is ACM?", answer: "ACM, or the Association for Computing Machinery, is the world's largest computing society that connects professionals, educators, and students to share resources and knowledge in the field of computing." },
+    { question: "How can I join ACM BITS Pilani Hyderabad?", answer: "You can join our ACM chapter by attending our orientation session at the beginning of the semester or by reaching out to any of our team members. Membership provides access to all our events, workshops, and a network of computing enthusiasts." },
+    { question: "What kind of events does ACM host?", answer: "We host a variety of events, including coding competitions, technical workshops, guest lectures, and collaborative projects. Our events cater to different skill levels, ensuring that everyone can participate and benefit." },
+    { question : "Do I need to have prior experience in computing to join?", answer : "No prior experience is necessary! Our chapter welcomes students from all backgrounds. Whether you're just starting or you're an advanced coder, there's something for everyone in our events and activities."}
   ];

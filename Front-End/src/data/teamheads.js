40e694df28076edd8e2ba20f56b6169598d666b8
Aravind Sathesh <aravind.sathesh@gmail.com>
2025-01-28 18:56:05 +0530

    Alternate Office page styling

--- Front-End/src/data/teamheads.js
+++ Front-End/src/data/teamheads.js
@@ -1,47 +1,47 @@
 export const teamHeads = [
-    {
-        name : "Shashwat Sharma",
-        designation: "Chairperson",
-        image : "/shaswat.png"
-    },
-    {
-        name : "Dhairya Luthra",
-        designation: "Vice Chairperson and App Dev Chair",
-        image : "/dhairya.png"
-    },
-    {
-        name : "Vivan Jain",
-        designation: "Quant Chair",
-        image : "/vivan.png"
-    },
-    {
-        name : "Samarth Pawan",
-        designation: "ML Chair",
-        image : "/samarth.png"
-    },
-    {
-        name : "Arinjay Jain",
-        designation: "Web Dev Chair",
-        image : "/arinjay.png"
-    },
-    {
-        name : "Tushar Anand",
-        designation: "Generative AI Chair",
-        image : "/tushar.png"
-    },
-    {
-        name : "Divya Daga",
-        designation: "Content Chair",
-        image : "/divya.png"
-    },
-    {
-        name : "Majji Sai Snigdha",
-        designation: "Events Chair",
-        image : "/majji.png"
-    },
-    {
-        name : "Lakshita Jain",
-        designation: "Design Chair",
-        image : "/lakshita.png"
-    }
-]
+	{
+		name: 'Shashwat Sharma',
+		designation: 'Chairperson',
+		image: '/shaswat.png',
+	},
+	{
+		name: 'Dhairya Luthra',
+		designation: 'Vice Chairperson, App Dev Chair',
+		image: '/dhairya.png',
+	},
+	{
+		name: 'Vivan Jain',
+		designation: 'Quant Chair',
+		image: '/vivan.png',
+	},
+	{
+		name: 'Samarth Pawan',
+		designation: 'ML Chair',
+		image: '/samarth.png',
+	},
+	{
+		name: 'Arinjay Jain',
+		designation: 'Web Dev Chair',
+		image: '/arinjay.png',
+	},
+	{
+		name: 'Tushar Anand',
+		designation: 'Generative AI Chair',
+		image: '/tushar.png',
+	},
+	{
+		name: 'Divya Daga',
+		designation: 'Content Chair',
+		image: '/divya.png',
+	},
+	{
+		name: 'Majji Sai Snigdha',
+		designation: 'Events Chair',
+		image: '/majji.png',
+	},
+	{
+		name: 'Lakshita Jain',
+		designation: 'Design Chair',
+		image: '/lakshita.png',
+	},
+];

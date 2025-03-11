d76d12dd71196b8a70785159266808e7a8b6c51e
DevanshAgarwal1806 <devansh180606@gmail.com>
2025-03-12 00:33:35 +0530

    added team-heads images

--- lib/team-heads.ts
+++ lib/team-heads.ts
@@ -2,47 +2,47 @@ export const teamHeads = [
   {
     name: "Shashwat Sharma",
     designation: "Chairperson",
-    image: "/shaswat.jpg",
+    image: "/teamMembers/shaswat.jpg",
   },
   {
     name: "Dhairya Luthra",
     designation: "Vice Chairperson, App Dev Chair",
-    image: "/dhairya.png",
+    image: "/teamMembers/dhairya.png",
   },
   {
     name: "Vivan Jain",
     designation: "Quant Chair",
-    image: "/vivan.png",
+    image: "/teamMembers/vivan.png",
   },
   {
     name: "Samarth Pawan",
     designation: "ML Chair",
-    image: "/samarth.jpg",
+    image: "/teamMembers/samarth.jpg",
   },
   {
     name: "Arinjay Jain",
     designation: "Web Dev Chair",
-    image: "/arinjay.jpg",
+    image: "/teamMembers/arinjay.jpg",
   },
   {
     name: "Tushar Anand",
     designation: "Generative AI Chair",
-    image: "/tushar.png",
+    image: "/teamMembers/tushar.png",
   },
   {
     name: "Divya Daga",
     designation: "Content Chair",
-    image: "/divya.jpg",
+    image: "/teamMembers/divya.jpg",
   },
   {
     name: "Majji Sai Snigdha",
     designation: "Events Chair",
-    image: "/majji.JPG",
+    image: "/teamMembers/majji.jpeg",
   },
   {
     name: "Lakshita Jain",
     designation: "Design Chair",
-    image: "/lakshita.jpg",
+    image: "/teamMembers/lakshita.jpg",
   },
 ]
 

d1ca9c40249a58934731fda9e0348b10bd15a33c
Aravind Sathesh <aravind.sathesh@gmail.com>
2025-03-12 19:46:59 +0530

    chore: update FAQs

--- lib/faq.ts
+++ lib/faq.ts
@@ -1,23 +1,18 @@
 export const faqItems = [
-  {
-    question: "What is ACM?",
-    answer:
-      "ACM, or the Association for Computing Machinery, is the world's largest computing society that connects professionals, educators, and students to share resources and knowledge in the field of computing.",
-  },
-  {
-    question: "How can I join ACM BITS Pilani Hyderabad?",
-    answer:
-      "You can join our ACM chapter by attending our orientation session at the beginning of the semester or by reaching out to any of our team members. Membership provides access to all our events, workshops, and a network of computing enthusiasts.",
-  },
-  {
-    question: "What kind of events does ACM host?",
-    answer:
-      "We host a variety of events, including coding competitions, technical workshops, guest lectures, and collaborative projects. Our events cater to different skill levels, ensuring that everyone can participate and benefit.",
-  },
-  {
-    question: "Do I need to have prior experience in computing to join?",
-    answer:
-      "No prior experience is necessary! Our chapter welcomes students from all backgrounds. Whether you're just starting or you're an advanced coder, there's something for everyone in our events and activities.",
-  },
-]
-
+	{
+		question: 'What is ACM?',
+		answer: "ACM, or the Association for Computing Machinery, is the world's largest computing society that connects professionals, educators, and students to share resources and knowledge in the field of computing.",
+	},
+	{
+		question: 'How can I join ACM BITS Pilani Hyderabad?',
+		answer: 'You can join our ACM chapter by attending our orientation session at the beginning of the semester or by reaching out to any of our team members. Membership provides access to all our events, workshops, and a network of computing enthusiasts.',
+	},
+	{
+		question: 'What kind of events does ACM host?',
+		answer: 'We host a variety of events, including coding competitions, technical workshops, guest lectures, and collaborative projects. Our events cater to different skill levels, ensuring that everyone can participate and benefit.',
+	},
+	{
+		question: 'Do I need to have prior experience to join?',
+		answer: "No prior experience is necessary! Our chapter welcomes students from all backgrounds. Whether you're just starting or you're an advanced coder, there's something for everyone in our events and activities.",
+	},
+];

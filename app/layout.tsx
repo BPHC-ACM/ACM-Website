0cc4daad8c893c4e394e1f84f729dd0bf883419b
Aravind Sathesh <aravind.sathesh@gmail.com>
2025-08-24 12:01:23 +0530

    chore(seo): add meta tags and opengraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,27 @@ export const metadata = {
 		icon: '/acm-logo.ico',
 		apple: '/acm-logo.png',
 	},
+	openGraph: {
+		title: 'ACM BPHC',
+		description:
+			'Association for Computing Machinery - BITS Pilani, Hyderabad Campus, Student Chapter',
+		images: [
+			{
+				url: '/og-image.png',
+				width: 1200,
+				height: 630,
+				alt: 'ACM BPHC OG Image',
+			},
+		],
+		type: 'website',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'ACM BPHC',
+		description:
+			'Association for Computing Machinery - BITS Pilani, Hyderabad Campus, Student Chapter',
+		images: ['/og-image.png'],
+	},
 };
 
 export default function RootLayout({
